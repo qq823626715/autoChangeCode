7b216af34d31c98f93cbb86fc854d5199d50ea37
LiuShengrong <liusr823626715@163.com>
2022-09-13 16:31:02 +0800

    refactor: 修改匹配规则，静止换行

--- rules.js
+++ rules.js
@@ -6,7 +6,7 @@ const changeRules = {
     
     // 增加 少部分中英文掺杂文本判断，以及句尾（元）等单位判定
     type: /\b[\w-]+\s*=\s*("([\s\w\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\(\)]*[\u4E00-\u9FA5]{1,}[\s\w\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\(\)]*)+"|'([\s\w\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\(\)]*[\u4E00-\u9FA5]{1,}[\s\w\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\(\)]*)+')/g,
-    reg1: /("([^"]*[\u4E00-\u9FA5]{1,}[^"]*)+")|('([^']*[\u4E00-\u9FA5]{1,}[^']*)+')/,
+    reg1: /("([^"\n]*[\u4E00-\u9FA5]{1,}[^"\n]*)+")|('([^'\n]*[\u4E00-\u9FA5]{1,}[^'\n]*)+')/,
     reg2: /^"|^'|"$|'$/g,
     sliceFlag: "=",
     changeContent: function(keyContent, prefix) {
@@ -25,8 +25,8 @@ const changeRules = {
   // 匹配第三种模式。<script></script>模块中的 "中文"或'中文'
   // 注意项目Eslint规则，需要 " 或 '
   scriptRule: {
-    type: /(([^!=]=)|:|\(|\+)\s*(("([^"]*[\u4E00-\u9FA5]{1,}[^"]*)+")|('([^']*[\u4E00-\u9FA5]{1,}[^']*)+'))(?!\s*(!=|==)=?)/g,
-    reg1: /(?<![\u4E00-\u9FA5])("([^"]*[\u4E00-\u9FA5]{1,}[^"]*)+")|('([^']*[\u4E00-\u9FA5]{1,}[^']*)+')/,
+    type: /(([^!=]=)|:|\(|\+)\s*(("([^"\n]*[\u4E00-\u9FA5]{1,}[^"\n]*)+")|('([^'\n]*[\u4E00-\u9FA5]{1,}[^'\n]*)+'))(?!\s*(!=|==)=?)/g,
+    reg1: /(?<![\u4E00-\u9FA5])("([^"\n]*[\u4E00-\u9FA5]{1,}[^"\n]*)+")|('([^'\n]*[\u4E00-\u9FA5]{1,}[^'\n]*)+')/,
     reg2: /^"|^'|'$|"$/g,
     changeContent: function(keyContent, prefix) {
       return `${prefix} this.$t('${keyContent}')`
